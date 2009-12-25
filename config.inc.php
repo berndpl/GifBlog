31d40aff44357c795dc8e4c27fe812441499d599
Bernd Plontsch <bernd.plontsch@gmail.com>
2009-12-25 19:09:00 +0100

    itemsperpage = 80

--- config.inc.php
+++ config.inc.php
@@ -5,7 +5,7 @@ $path_file = pathinfo($_SERVER["SCRIPT_FILENAME"]);
 $mediafolder = '/pub';
 $path = $path_file ['dirname'].$mediafolder;
 
-$itemsperpage = 10;
+$itemsperpage = 80;
 $to = 9999; //max entries to show
 
 ?>
